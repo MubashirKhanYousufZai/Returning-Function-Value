3473ad08199f06f2f407090761ae6965609889db
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-04-09 17:16:34 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+"use strict";
+// Returning Function Value
+function finalTestMarks() {
+    let maths = 10 + 20 + 43;
+    // maths + english + urdu
+    return maths;
+}
+let result = finalTestMarks();
+console.log(result);
+function makeCoffee() {
+    let ingredients = 1 + 1 + 2;
+    // milk + coffee + sugar
+    return ingredients;
+}
+let done = makeCoffee();
+console.log(done);
+function calculateArea() {
+    let measurment = 6 * 3;
+    // length = 6 , width = 3
+    return measurment;
+}
+let area = calculateArea();
+console.log(area);
---
+++ main.ts
@@ -0,0 +1,24 @@
+// Returning Function Value
+function finalTestMarks(): number {
+    let maths = 10 + 20 + 43;
+    // maths + english + urdu
+    return maths
+}
+let result: number = finalTestMarks()
+console.log(result);
+
+function makeCoffee(): number {
+    let ingredients = 1 + 1 + 2 ;
+    // milk + coffee + sugar
+    return ingredients
+}
+let done: number = makeCoffee()
+console.log(done);
+
+function calculateArea(): number {
+    let measurment = 6 * 3;  
+    // length = 6 , width = 3
+    return measurment                                                                                     
+}
+let area: number = calculateArea()
+console.log(area);
